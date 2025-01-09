3e71482f389615896c2cc7096820bb15993d91e6
Adam Danicki <adanicki@umass.edu>
2025-01-08 20:08:55 -0500

    Initial Setup

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+//==================================================//
+//  Main Backend file for Music Recommender Website
+//==================================================//
+
+// Load .env variables
+const dotenv = require('dotenv')
+dotenv.config();
+
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET
+
+
